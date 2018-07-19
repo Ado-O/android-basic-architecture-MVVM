fe66aed2cd8c864c940579a585e342766998972f
Denis <berilodenis@gmail.com>
2018-07-19 17:00:39 +0200

    Fragment

--- app/src/main/java/com/tech387/arch/MainActivity.java
+++ app/src/main/java/com/tech387/arch/MainActivity.java
@@ -1,23 +1,14 @@
 package com.tech387.arch;
 
-import android.support.v4.app.Fragment;
-import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.support.v7.widget.LinearLayoutManager;
-import android.support.v7.widget.RecyclerView;
 import android.support.v7.widget.Toolbar;
-import android.widget.ListView;
 
-import com.tech387.arch.data.Movie;
-
-import java.util.ArrayList;
+import com.tech387.arch.util.ActivityUtils;
 
 public class MainActivity extends AppCompatActivity {
 
     private Toolbar mToolbar;
-    private ViewPager mViewPager;
-    private MainAdapter mAdapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -26,11 +17,11 @@ public class MainActivity extends AppCompatActivity {
 
         //Finding views
         mToolbar = findViewById(R.id.toolbar);
-        mViewPager = findViewById(R.id.pager);
+
 
         //Setup
         setupToolbar();
-        setupPager();
+        setupFragment();
     }
 
     /**
@@ -58,14 +49,11 @@ public class MainActivity extends AppCompatActivity {
     /**
      * Setting up the listView & its adapter
      */
-    private void setupPager() {
-        //Mocking the data
-        ArrayList<Fragment> arrayList = new ArrayList<>();
-        arrayList.add(MainOneFragment.newInstance());
-        arrayList.add(MainTwoFragment.newInstance());
-
-        mAdapter = new MainAdapter(getSupportFragmentManager(), arrayList);
-        mViewPager.setAdapter(mAdapter);
-
+    private void setupFragment() {
+        ActivityUtils.replaceFragmentInActivity(
+                getSupportFragmentManager(),
+                MainOneFragment.newInstance(),
+                R.id.container
+        );
     }
 }
---
+++ app/src/main/java/com/tech387/arch/util/ActivityUtils.java
@@ -0,0 +1,28 @@
+package com.tech387.arch.util;
+
+import android.support.annotation.NonNull;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentTransaction;
+
+
+public class ActivityUtils {
+
+    public static void replaceFragmentInActivity(@NonNull FragmentManager fragmentManager,
+                                                 @NonNull Fragment fragment, int frameId) {
+
+        FragmentTransaction transaction = fragmentManager.beginTransaction();
+        transaction.replace(frameId, fragment);
+        transaction.commit();
+    }
+
+    public static void replaceFragmentInActivity(@NonNull FragmentManager fragmentManager,
+                                                 @NonNull Fragment fragment, String tag) {
+
+        FragmentTransaction transaction = fragmentManager.beginTransaction();
+        transaction.add(fragment, tag);
+        transaction.commit();
+    }
+
+
+}
