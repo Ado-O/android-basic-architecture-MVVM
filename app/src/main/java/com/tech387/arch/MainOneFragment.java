9d3e2d9d0f51a9e9d6f899a4b1b9fb5e5110c724
Denis <berilodenis@gmail.com>
2018-07-19 14:32:53 +0200

    View pager

---
+++ app/src/main/java/com/tech387/arch/MainOneFragment.java
@@ -0,0 +1,26 @@
+package com.tech387.arch;
+
+import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+public class MainOneFragment extends Fragment {
+
+    public static MainOneFragment newInstance() {
+
+
+        return new MainOneFragment();
+    }
+
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.main_one_frag, container, false);
+
+        return view;
+    }
+}
---
+++ app/src/main/java/com/tech387/arch/MainTwoFragment.java
@@ -0,0 +1,24 @@
+package com.tech387.arch;
+
+import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+public class MainTwoFragment extends Fragment {
+
+    public static MainTwoFragment newInstance() {
+        return new MainTwoFragment();
+    }
+
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.main_two_frag, container, false);
+
+        return view;
+    }
+}
